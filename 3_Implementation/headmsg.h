51734298577a82668489614798d433f2b1d1b7f3
Tanya-M-Vattathil <101464288+Tanya-M-Vattathil@users.noreply.github.com>
2022-04-02 10:04:18 +0530

    Update headmsg.h

--- 3_Implementation/headmsg.h
+++ 3_Implementation/headmsg.h
@@ -1,7 +1,7 @@
 #ifndef HEADMSG_H
 #define HEADMSG_H
 
-
+//creates a heading message to be shown at the beginning of each choices
 void headMessage(const char *message){
     system("cls");
     printMessageCenter(message);
